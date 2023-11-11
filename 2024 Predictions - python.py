8631616c264432e985086c727092f9d838653662
briank86 <143430894+briank86@users.noreply.github.com>
2023-11-11 11:52:29 -0500

    Update 2024 Predictions - python.py

--- 2024 Predictions - python.py
+++ 2024 Predictions - python.py
@@ -107,7 +107,7 @@ def stats2024(player):
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
     
-        # Create and train the XGBoost model
+        # Create and train the RandomForestRegressor model
         clf = RandomForestRegressor(n_estimators=100, random_state=42)
         clf.fit(X_train, y_train)
     
